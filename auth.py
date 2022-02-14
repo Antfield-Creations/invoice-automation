efcd42fec53f729e3d0376407f05c341773598b1
reinvantveer <reinvantveer@gmail.com>
2022-02-14 11:05:39 +0100

    add authentication/authorization module
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

---
+++ auth.py
@@ -0,0 +1,40 @@
+import os
+from typing import Optional
+
+from google.auth.transport import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+from config import Config
+
+
+def get_credentials(config: Config) -> Credentials:
+    # Start with empty credentials
+    creds: Optional[Credentials] = None
+
+    # Load token from previous session if present
+    if os.path.exists(config['oauth']['token_path']):
+        creds = Credentials.from_authorized_user_file(
+            filename=config['oauth']['token_path'],
+            scopes=config['oauth']['scopes']
+        )
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            # Connect to the Google docs service
+            flow = InstalledAppFlow.from_client_secrets_file(
+                client_secrets_file=config['oauth']['credentials_path'],
+                scopes=config['oauth']['scopes']
+            )
+
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open(config['oauth']['token_path'], 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
